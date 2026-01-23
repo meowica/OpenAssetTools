ac6c0dec4d071561f3f2b60fcb09bbb9c620f19c
hindercanrun <109132519+meowica@users.noreply.github.com>
2026-01-23 13:14:23 +0800

    add AddonMapEnts dumper

---
+++ src/ObjWriting/Game/T6/Maps/AddonMapEntsDumperT6.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "Dumping/AbstractAssetDumper.h"
+#include "Game/T6/T6.h"
+
+namespace addon_map_ents
+{
+    class DumperT6 final : public AbstractAssetDumper<T6::AssetAddonMapEnts>
+    {
+    public:
+        explicit DumperT6(const AssetPool<T6::AssetAddonMapEnts::Type>& pool);
+
+    protected:
+        void DumpAsset(AssetDumpingContext& context, const XAssetInfo<T6::AssetAddonMapEnts::Type>& asset) override;
+    };
+} // namespace addon_map_ents
