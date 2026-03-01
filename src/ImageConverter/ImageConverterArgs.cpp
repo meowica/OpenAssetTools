df9badbf349ac57f8a1962cff1481e78597de7fc
hindercanrun <109132519+meowica@users.noreply.github.com>
2026-03-01 15:01:36 +0800

    Remove other games from ImageConverter

--- src/ImageConverter/ImageConverterArgs.cpp
+++ src/ImageConverter/ImageConverterArgs.cpp
@@ -34,55 +34,14 @@ const CommandLineOption* const OPTION_NO_COLOR =
     .WithLongName("no-color")
     .WithDescription("Disables colored terminal output.")
     .Build();
-
-constexpr auto CATEGORY_GAME = "Game";
-
-const CommandLineOption* const OPTION_GAME_IW3 =
-    CommandLineOption::Builder::Create()
-    .WithLongName("iw3")
-    .WithCategory(CATEGORY_GAME)
-    .WithDescription("Converts images for IW3.")
-    .Build();
-
-const CommandLineOption* const OPTION_GAME_IW4 =
-    CommandLineOption::Builder::Create()
-    .WithLongName("iw4")
-    .WithCategory(CATEGORY_GAME)
-    .WithDescription("Converts images for IW4.")
-    .Build();
-
-const CommandLineOption* const OPTION_GAME_IW5 =
-    CommandLineOption::Builder::Create()
-    .WithLongName("iw5")
-    .WithCategory(CATEGORY_GAME)
-    .WithDescription("Converts images for IW5.")
-    .Build();
-
-const CommandLineOption* const OPTION_GAME_T5 =
-    CommandLineOption::Builder::Create()
-    .WithLongName("t5")
-    .WithCategory(CATEGORY_GAME)
-    .WithDescription("Converts images for T5.")
-    .Build();
-
-const CommandLineOption* const OPTION_GAME_T6 =
-    CommandLineOption::Builder::Create()
-    .WithLongName("t6")
-    .WithCategory(CATEGORY_GAME)
-    .WithDescription("Converts images for T6.")
-    .Build();
 // clang-format on
 
-const CommandLineOption* const COMMAND_LINE_OPTIONS[]{
+const CommandLineOption* const COMMAND_LINE_OPTIONS[]
+{
     OPTION_HELP,
     OPTION_VERSION,
     OPTION_VERBOSE,
     OPTION_NO_COLOR,
-    OPTION_GAME_IW3,
-    OPTION_GAME_IW4,
-    OPTION_GAME_IW5,
-    OPTION_GAME_T5,
-    OPTION_GAME_T6,
 };
 
 ImageConverterArgs::ImageConverterArgs()
@@ -152,9 +111,6 @@ bool ImageConverterArgs::ParseArgs(const int argc, const char** argv, bool& shou
 
     // --no-color
     con::set_use_color(!m_argument_parser.IsOptionSpecified(OPTION_NO_COLOR));
-
-    if (m_argument_parser.IsOptionSpecified(OPTION_GAME_T6))
-        m_game_to_convert_to = GameId::T6;
 
     return true;
 }
